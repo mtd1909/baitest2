516878b9e95b30bb21bdef751c03c1c83b38601b
Mai Thanh Dat <datmt@timebird.org>
2023-05-17 18:07:48 +0700

    push

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  header.classList.toggle('sticky', window.scrollY > 64);
+});
+
+const headerBars = document.querySelector('.header__bars')
+const overlay = document.querySelector('.header__overlay')
+const headerRight = document.querySelector('.header__right')
+const body = document.querySelector('body')
+
+headerBars.addEventListener('click', () => {
+  overlay.classList.toggle('show-overlay')
+  headerRight.classList.toggle('show-menu')
+  body.classList.toggle('overflow')
+})
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('show-overlay')
+  headerRight.classList.remove('show-menu')
+  body.classList.remove('overflow')
+})
